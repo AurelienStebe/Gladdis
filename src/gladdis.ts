0e1086f62d3239e7046d0b1ff74a327b6fc4578e
Aurélien Stébé <aurelien.stebe@gmail.com>
2023-04-15 12:31:58 -0400

    Switch OpenAI API lib and add TikToken count modal

--- src/gladdis.ts
+++ src/gladdis.ts
@@ -2,22 +2,20 @@ import path from 'path'
 import yaml from 'yaml'
 import merge from 'deepmerge'
 import { promises as fs } from 'fs'
-import { OpenAIChat } from 'langchain/llms'
-import { CallbackManager } from 'langchain/callbacks'
+import { Tiktoken } from '@dqbd/tiktoken/lite'
+import { OpenAIClient } from '@fern-api/openai'
 import { transcribe } from './whisper.js'
 
 import type { ChatCompletionRequestMessage } from 'openai'
 import type { Context } from './types.js'
 
+import cl100k_base from '@dqbd/tiktoken/encoders/cl100k_base.json' assert { type: 'json' }
+
+const openai = new OpenAIClient({ token: process.env.OPENAI_API_KEY ?? '' })
+
 export async function askGladdis(context: Context): Promise<void> {
     context = await transcribe(context)
 
-    await fs.appendFile(context.file.path, `\n\n**${context.gladdis.label}:** `)
-    await chatWithGladdis(context)
-    await fs.appendFile(context.file.path, `\n\n---\n\n**${context.user.label}:** `)
-}
-
-export async function chatWithGladdis(context: Context): Promise<void> {
     const chatContext = merge({}, context) as any
 
     delete chatContext.file
@@ -25,34 +23,71 @@ export async function chatWithGladdis(context: Context): Promise<void> {
     delete chatContext.gladdis
     delete chatContext.whisper
 
+    const promptMessage: ChatCompletionRequestMessage = {
+        role: 'user',
+        content: context.user.prompt,
+        name: context.user.label,
+    }
+
     const prefixMessages: ChatCompletionRequestMessage[] = [
         { role: 'system', content: context.gladdis.corePrompt },
         { role: 'system', content: context.gladdis.metaPrompt + JSON.stringify(chatContext) },
     ]
 
+    context.user.history.push(promptMessage)
     context.user.history.unshift(...prefixMessages)
 
-    const gladdis = new OpenAIChat({
-        prefixMessages: context.user.history,
-        modelName: context.gladdis.model,
-        temperature: context.gladdis.temperature,
-        topP: context.gladdis.top_p_param,
-        frequencyPenalty: context.gladdis.freq_penalty,
-        presencePenalty: context.gladdis.pres_penalty,
-        cache: false,
-        streaming: true,
-        callbackManager: CallbackManager.fromHandlers({
-            async handleLLMNewToken(token) {
-                await fs.appendFile(context.file.path, token)
-            },
-        }),
+    context = await chatWithGladdis(context)
+
+    void logGladdisCall(context)
+    void logGladdisChat(context)
+}
+
+export async function chatWithGladdis(context: Context): Promise<Context> {
+    const gladdisResponse: ChatCompletionRequestMessage = {
+        role: 'assistant',
+        content: '',
+    }
+
+    let deferredPromise: (value: string) => void
+    const finishMessage = new Promise<string>((resolve) => {
+        deferredPromise = resolve
     })
 
-    context.user.history.push({ role: 'user', content: context.user.prompt, name: context.user.label })
-    context.user.history.push({ role: 'assistant', content: await gladdis.call(context.user.prompt) })
+    await openai.chat.createCompletion(
+        {
+            stream: true,
+            model: context.gladdis.model,
+            messages: context.user.history,
+            temperature: context.gladdis.temperature,
+            topP: context.gladdis.top_p_param,
+            frequencyPenalty: context.gladdis.freq_penalty,
+            presencePenalty: context.gladdis.pres_penalty,
+        },
+        (data) => {
+            if (data.choices[0].delta.role === 'assistant') {
+                void fs.appendFile(context.file.path, `\n\n**${context.gladdis.label}:** `)
+            }
+            if (data.choices[0].delta.content !== undefined) {
+                gladdisResponse.content += data.choices[0].delta.content
+                void fs.appendFile(context.file.path, data.choices[0].delta.content)
+            }
+        },
+        {
+            onFinish: () => {
+                context.user.history.push(gladdisResponse)
+                deferredPromise(`${getTokenModal(context)}\n\n---\n\n**${context.user.label}:** `)
+            },
+            onError: (error) => {
+                context.user.history.push(gladdisResponse)
+                deferredPromise(`${error?.toString() ?? '**Error**'}\n\n---\n\n**${context.user.label}:** `)
+            },
+        }
+    )
 
-    await logGladdisCall(context)
-    await logGladdisChat(context)
+    await fs.appendFile(context.file.path, await finishMessage)
+
+    return context
 }
 
 async function logGladdisCall(context: Context): Promise<void> {
@@ -114,4 +149,28 @@ function writeHistory(context: Context): string {
     })
 
     return history.join('\n\n') + '\n'
+}
+
+function getTokenModal(context: Context): string {
+    const tokenLength = getTokenLength(context.user.history)
+
+    let tokenLimit = context.gladdis.model.startsWith('gpt-4') ? 8192 : 4096
+    if (context.gladdis.model.startsWith('gpt-4-32k')) tokenLimit = 32768
+
+    const tokenRatio = Math.ceil((tokenLength / tokenLimit) * 33)
+    const tokenGraph = `[**${'#'.repeat(tokenRatio)}**${'-'.repeat(33 - tokenRatio)}]`
+    const tokenCount = `**${tokenLength.toLocaleString()}** tokens out of ${tokenLimit.toLocaleString()}`
+
+    return `\n\n> [!INFO]\n> Using ${tokenCount} max tokens.\n>\n> ${tokenGraph}`
+}
+
+function getTokenLength(messages: ChatCompletionRequestMessage[]): number {
+    const tiktoken = new Tiktoken(cl100k_base.bpe_ranks, cl100k_base.special_tokens, cl100k_base.pat_str)
+
+    const fullHistory = messages.map((message) => `${message.name ?? message.role}\n${message.content}`)
+    const tokenLength = tiktoken.encode(fullHistory.join('\n')).length + messages.length * 3
+
+    tiktoken.free()
+
+    return tokenLength
 }
