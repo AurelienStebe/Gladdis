cbcebf99d0317db979ac1bc539670de8eca36e39
Aurélien Stébé <aurelien.stebe@gmail.com>
2024-11-29 15:09:56 -0400

    build: cleanup the NPM scripts & update all deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     {
-        ignores: ['main.js', 'lib/', 'types/', 'coverage/'],
+        ignores: ['main.js', 'lib/', 'types/', 'coverage/', 'vault/'],
     },
     eslint.configs.recommended,
     {
