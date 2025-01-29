f39d229f1b57cfdf3f5d076e4c62ac8f8ac0c55a
Aurélien Stébé <aurelien.stebe@gmail.com>
2025-01-29 15:22:19 -0400

    build: add "themes" to gitignore and clean script

--- version-bump.js
+++ version-bump.js
@@ -4,15 +4,15 @@ import { tar, zip } from 'zip-a-folder'
 
 const targetVersion = process.argv[2] ?? process.env.npm_package_version
 
-// read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(fs.readFileSync('manifest.json', 'utf-8'))
-const { minAppVersion } = manifest
-manifest.version = targetVersion
-fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, '    ') + '\n')
+if (targetVersion !== undefined) {
+    const manifest = JSON.parse(fs.readFileSync('manifest.json', 'utf-8'))
+    const { minAppVersion } = manifest
+    manifest.version = targetVersion
+    fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 4) + '\n')
 
-// update versions.json with target version and minAppVersion from manifest.json
-const versions = JSON.parse(fs.readFileSync('versions.json', 'utf-8'))
-versions[targetVersion] = minAppVersion
-fs.writeFileSync('versions.json', JSON.stringify(versions, null, '    ') + '\n')
+    const versions = JSON.parse(fs.readFileSync('versions.json', 'utf-8'))
+    versions[targetVersion] = minAppVersion
+    fs.writeFileSync('versions.json', JSON.stringify(versions, null, 4) + '\n')
+}
 
 tar('vault', 'vault.tgz').then(() => zip('vault', 'vault.zip').then(() => process.exit(0)))
